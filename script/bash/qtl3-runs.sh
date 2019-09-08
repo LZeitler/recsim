c9451507e4d332cd2f6422b66f8cf31250bfa7f6
Leo Zeitler <leo.zeitler@gmail.com>
2019-09-08 18:50:34 +0200

    was missing

--- script/bash/qtl3-runs.sh
+++ script/bash/qtl3-runs.sh
@@ -4,6 +4,6 @@
 
 
 for i in {1..10}; do
-    $HOME/programs/SLiM_build/slim $HOME/pro/recsim/script/eidos/qtl3.E
+    $HOME/programs/SLiM_build/slim $HOME/pro/recsim/script/eidos/qtl3.E # 3-4 map
 done
 
