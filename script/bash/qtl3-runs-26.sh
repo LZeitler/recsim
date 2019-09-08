3c517ff1f2b913c115033200c4cc842c3fb3fee0
Leo Zeitler <leo.zeitler@gmail.com>
2019-09-08 18:47:07 +0200

    changes for other maps

---
+++ script/bash/qtl3-runs-26.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# cd $1
+
+
+for i in {1..10}; do
+    $HOME/programs/SLiM_build/slim $HOME/pro/recsim/script/eidos/qtl3-26.E # 2-6 map
+done
+
---
+++ script/bash/qtl3-runs-42.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# cd $1
+
+
+for i in {1..10}; do
+    $HOME/programs/SLiM_build/slim $HOME/pro/recsim/script/eidos/qtl3-42.E # 2-6 map
+done
+
