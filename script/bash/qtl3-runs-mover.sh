d34f8146d54a827839e6b3510f449be088cfedf6
Leo Zeitler <leo.zeitler@gmail.com>
2019-09-25 17:17:37 +0200

    mover new runs

--- script/bash/qtl3-runs-mover.sh
+++ script/bash/qtl3-runs-mover.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-dir=1900080919 # 1808080919  1855080919  1900080919
+# dir=1900080919 # 1808080919  1855080919  1900080919  150cM runs
+dir=1520250919 			# 1518250919 1516250919 75cM runs
 filename=qtl3-phenotypes.txt
 
 cd /cluster/scratch/zeitlerl/slim/$dir
