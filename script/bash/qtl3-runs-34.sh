55eae2eac090f21f5928b280e789452531aff7e3
lzeitler <leo.zeitler@gmail.com>
2019-10-01 18:41:00 +0200

    new submitter and renamed

---
+++ script/bash/qtl3-runs-34.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# cd $1
+
+
+for i in {1..10}; do
+    $HOME/programs/SLiM_build/slim $HOME/pro/recsim/script/eidos/qtl3.E # 3-4 map
+done
+
--- script/bash/qtl3-runs-submitter.sh
+++ script/bash/qtl3-runs-submitter.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-wdir=$SCRATCH/slim/$(date +%H%M%S%d%m%y)/
+wdirb=$SCRATCH/slim/$(date +%H%M%S%d%m%y)
+
+wdir=$wdirb.26
 mkdir -p $wdir
 cd $wdir
-bsub -J "qtl3-1[1-100]" -n 1 -W 24:00 -R "rusage[mem=4000]" -oo $HOME/logs/%J_%I.stdout -eo $HOME/logs/%J_%I.stderr '$HOME/pro/recsim/script/bash/qtl3-runs-26.sh $wdir'
-# bsub -J "qtl3-3[1-100]" -n 1 -W 24:00 -R "rusage[mem=4000]" -oo $HOME/logs/%J_%I.stdout -eo $HOME/logs/%J_%I.stderr '$HOME/pro/recsim/script/bash/qtl3-runs-42.sh $wdir'
-# bsub -J "qtl3-2[1-100]" -n 1 -W 24:00 -R "rusage[mem=4000]" -oo $HOME/logs/%J_%I.stdout -eo $HOME/logs/%J_%I.stderr '$HOME/pro/recsim/script/bash/qtl3-runs.sh $wdir'
+bsub -J "qtl326[1-100]" -n 1 -W 24:00 -R "rusage[mem=4000]" -oo $HOME/logs/%J_%I.stdout -eo $HOME/logs/%J_%I.stderr '$HOME/pro/recsim/script/bash/qtl3-runs-26.sh $wdir'
+
+wdir=$wdirb.42
+mkdir -p $wdir
+cd $wdir
+bsub -J "qtl342[1-100]" -n 1 -W 24:00 -R "rusage[mem=4000]" -oo $HOME/logs/%J_%I.stdout -eo $HOME/logs/%J_%I.stderr '$HOME/pro/recsim/script/bash/qtl3-runs-42.sh $wdir'
+
+wdir=$wdirb.34
+mkdir -p $wdir
+cd $wdir
+bsub -J "qtl334[1-100]" -n 1 -W 24:00 -R "rusage[mem=4000]" -oo $HOME/logs/%J_%I.stdout -eo $HOME/logs/%J_%I.stderr '$HOME/pro/recsim/script/bash/qtl3-runs-34.sh $wdir'
